ddeee143fbbd42fb9a34e22b9bc3ef8d2841b4b3
rrichardknott <67433117+rrichardknott@users.noreply.github.com>
2020-07-16 12:33:50 -0700

    Code testing
    updated the code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,49 @@
-﻿$(document).ready(function () {
+﻿
 
-    $("#errorAlert").hide();
-    $("#errorAlertButton").hide();
+$("#errorAlert").hide();
+$("#errorAlertButton").hide();
 
-    $("#submitButton").click(function () {
-        let kNumber = $("#kNumber").val();
-        let firstNumber = $("#firstNumber").val();
-        let secondNumber = $("#secondNumber").val();
-        let thirdNumber = $("#thirdNumber").val();
-        let fourthNumber = $("#fourthNumber").val();
+$("#submitButton").click(function () {    
+    var kNumber = $("#kNumber").val();
+    var firstNumber = $("#firstNumber").val();
+    var secondNumber = $("#secondNumber").val();
+    var thirdNumber = $("#thirdNumber").val();
+    var fourthNumber = $("#fourthNumber").val();
 
-        // first check to see if user left any fields blank
-        if (kNumber.length == 0 || firstNumber.length == 0 || secondNumber.length == 0 || thirdNumber.length == 0 || fourthNumber.length == 0) {
-            $("#errorAlertButton").show();
-            $("#submitButton").hide();
-            $("#clearButton").hide();
-            $("#errorAlert").show().text("Did you forget to enter a number?");
-            return;
-        }
+    // first check to see if user left any fields blank
+    if (kNumber.length == 0 || firstNumber.length == 0 || secondNumber.length == 0 || thirdNumber.length == 0 || fourthNumber.length == 0) {
+        $("#errorAlertButton").show();
+        $("#submitButton").hide();
+        $("#clearButton").hide();
+        $("#errorAlert").show().text("Did you forget to enter a number?");
+        return;
+    }
+    // second check to see if all fields contain an integer
+    else if (isNaN(kNumber) || isNaN(firstNumber) || isNaN(secondNumber) || isNaN(thirdNumber) || isNaN(fourthNumber)) {
+        $("#errorAlertButton").show();
+        $("#submitButton").hide();
+        $("#clearButton").hide();
+        $("#errorAlert").show().text("Invalid Entry (numbers only)");
+        return;
+    } else {
+        var numbers = [firstNumber, secondNumber, thirdNumber, fourthNumber];
+        var answer = kNumber;
+        var checkValue = 0;
+        var found = false;
 
-        // second check to see if all fields contain an integer
-        if (isNaN(kNumber) || isNaN(firstNumber) || isNaN(secondNumber) || isNaN(thirdNumber) || isNaN(fourthNumber)) {
-            $("#errorAlertButton").show();
-            $("#submitButton").hide();
-            $("#clearButton").hide();
-            $("#errorAlert").show().text("Invalid Entry (numbers only)");
-            return;
-        }
-        else {
-            // create the array 
-            var userArray = [kNumber, firstNumber, secondNumber, thirdNumber, fourthNumber];
-    
-            // perform the calculation
-            //Step 3: Perform the algo
-            let message = "";
-
-            //for (number in array) {
-            for (let i = 0; i < userArray.length - 1; i++) {
-                //If any number in the array is larger than K we can ignore it
-                if (userArray[i] > kNumber)
-                    continue;
-
-                let solution = kNumber - userArray[i]; //if K = 17 and number = 7 we have to find 10
-
-                if (userArray.indexOf(solution, i + 1) > -1) {
-                    message = `A solution was found for a K value of ${kNumber}`;
-                    break;
-                }
-                else {
-                    message = `No solution for a K value of ${kNumber} was found`;
-                }
+        for (let i = 0; i <= numbers.length - 1; i++) {
+            checkValue = answer - numbers[i];
+            if (numbers.indexOf(checkValue, i + 1) > -1) {
+                found = true;
+                return found
+                break;
             }
-
-            //Step 4: Print result to user
-            alert(message);
-            //$("#soafOut").text(message);
-   
         }
-    });
+        return (found);
+        $("#solveResults").text(found);
+    }
+});
+    
 
     $("#errorAlertButton").on("click", function () {
         $("#errorAlertButton").hide();
@@ -78,6 +64,3 @@
         $("#thirdNumber").val("");
         $("#fourthNumber").val("");
     });
-   
-
-});
