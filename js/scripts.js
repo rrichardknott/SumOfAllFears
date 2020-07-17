c0b088dacb4c6c1d055299af77011edca0e0691f
rrichardknott <67433117+rrichardknott@users.noreply.github.com>
2020-07-17 08:41:12 -0700

    updated the code
    Made it work!

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,68 @@
-﻿
+﻿$(document).ready(function () {    
+    $("#errorAlert").hide();
+    $("#errorAlertButton").hide();
 
-$("#errorAlert").hide();
-$("#errorAlertButton").hide();
+    $("#submitButton").click(function () {        
+        var kNumber = parseInt($("#kNumber").val());
+        var firstNumber = parseInt($("#firstNumber").val());
+        var secondNumber = parseInt($("#secondNumber").val());
+        var thirdNumber =parseInt($("#thirdNumber").val());
+        var fourthNumber = parseInt($("#fourthNumber").val());
 
-$("#submitButton").click(function () {    
-    var kNumber = $("#kNumber").val();
-    var firstNumber = $("#firstNumber").val();
-    var secondNumber = $("#secondNumber").val();
-    var thirdNumber = $("#thirdNumber").val();
-    var fourthNumber = $("#fourthNumber").val();
-
-    // first check to see if user left any fields blank
-    if (kNumber.length == 0 || firstNumber.length == 0 || secondNumber.length == 0 || thirdNumber.length == 0 || fourthNumber.length == 0) {
-        $("#errorAlertButton").show();
-        $("#submitButton").hide();
-        $("#clearButton").hide();
-        $("#errorAlert").show().text("Did you forget to enter a number?");
-        return;
-    }
-    // second check to see if all fields contain an integer
-    else if (isNaN(kNumber) || isNaN(firstNumber) || isNaN(secondNumber) || isNaN(thirdNumber) || isNaN(fourthNumber)) {
-        $("#errorAlertButton").show();
-        $("#submitButton").hide();
-        $("#clearButton").hide();
-        $("#errorAlert").show().text("Invalid Entry (numbers only)");
-        return;
-    } else {
-        var numbers = [firstNumber, secondNumber, thirdNumber, fourthNumber];
-        var answer = kNumber;
-        var checkValue = 0;
-        var found = false;
-
-        for (let i = 0; i <= numbers.length - 1; i++) {
-            checkValue = answer - numbers[i];
-            if (numbers.indexOf(checkValue, i + 1) > -1) {
-                found = true;
-                return found
-                break;
-            }
+        // first check to see if user left any fields blank
+        if (kNumber.length == 0 || firstNumber.length == 0 || secondNumber.length == 0 || thirdNumber.length == 0 || fourthNumber.length == 0) {
+            $("#errorAlertButton").show();
+            $("#submitButton").hide();
+            $("#clearButton").hide();
+            $("#errorAlert").show().text("Did you forget to enter a number?");
+            return;
         }
-        return (found);
-        $("#solveResults").text(found);
-    }
+        // second check to see if all fields contain an integer
+        else if (isNaN(kNumber) || isNaN(firstNumber) || isNaN(secondNumber) || isNaN(thirdNumber) || isNaN(fourthNumber)) {
+            $("#errorAlertButton").show();
+            $("#submitButton").hide();
+            $("#clearButton").hide();
+            $("#errorAlert").show().text("Invalid Entry (numbers only)");
+            return;
+        } else {            
+            numbers = [firstNumber, secondNumber, thirdNumber, fourthNumber];            
+            var checkValue = 0;             
+            for (let i = 0; i <= numbers.length - 1; i++) {
+                checkValue = kNumber - numbers[i];
+                if (numbers.includes(checkValue, i + 1)) {                    
+                    found = true;
+                    var sweet = swal({
+                        title: "Numbers Found!",
+                        text: `There were two numbers within your array of numbers that added up to your Sum Number of ${kNumber}: ${checkValue} and ${numbers[i]}.`,
+                        icon: "success",
+                    });
+                    break;
+                } else {
+                    found = false;
+                    var sweet = swal(`Nope, didn't happen.", "No numbers found to add up to your Sum Number of: ${kNumber}.`);
+                }                
+            }
+            return (found, sweet);
+        }
+    });    
+
+        $("#errorAlertButton").on("click", function () {
+            $("#errorAlertButton").hide();
+            $("#kNumber").val("").focus();
+            $("#firstNumber").val("");
+            $("#secondNumber").val("");
+            $("#thirdNumber").val("");
+            $("#fourthNumber").val("");
+            $("#submitButton").show();
+            $("#clearButton").show();        
+            $("#errorAlert").text("").hide();
+        });
+
+        $("#clearButton").on("click", function () {
+            $("#kNumber").val("").focus();    
+            $("#firstNumber").val("");
+            $("#secondNumber").val("");
+            $("#thirdNumber").val("");
+            $("#fourthNumber").val("");
+        });
 });
-    
-
-    $("#errorAlertButton").on("click", function () {
-        $("#errorAlertButton").hide();
-        $("#kNumber").val("").focus();
-        $("#firstNumber").val("");
-        $("#secondNumber").val("");
-        $("#thirdNumber").val("");
-        $("#fourthNumber").val("");
-        $("#submitButton").show();
-        $("#clearButton").show();        
-        $("#errorAlert").text("").hide();
-    });
-
-    $("#clearButton").on("click", function () {
-        $("#kNumber").val("").focus();    
-        $("#firstNumber").val("");
-        $("#secondNumber").val("");
-        $("#thirdNumber").val("");
-        $("#fourthNumber").val("");
-    });
